06d36a4664cbb86fee16257cee9fd859a015ea1d
qanszer <maluendajohnsteven@gmail.com>
2026-05-15 12:31:55 +0800

    feat: add UI to the program
    features:
    - basic interface (dark theme)
    - rock, paper, scissors buttons for user
    - restart button appears when user/computer wins

--- scripts.js
+++ scripts.js
@@ -1,81 +1,169 @@
-let humanSelection = getUserChoice();
-let computerSelection = getComputerChoice();
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+
+let message = document.createElement("p");
+let announcer = document.querySelector(".announcer");
+let player = document.querySelector(".player-score");
+let computer = document.querySelector(".computer-score");
+
+let computerSelection = "";
+
+let humanScore = 0;
+let computerScore = 0;
+let reset = 0;
+
+rock.addEventListener('click', () => {
+    let result = handleChoice("rock");
+    getComputerChoice();
+
+    console.log(result);
+    announcer.textContent = result;
+    player.textContent = humanScore;
+    computer.textContent = computerScore;
+
+    // rock.style.backgroundColor = "#ffcd66";
+    // if (computerSelection === "rock")
+    //     paper.style.color = "#769f45";
+    // if (computerSelection === "paper")
+    //     paper.style.color = "#769f45";
+    // if (computerSelection === "scissors")
+    //     paper.style.color = "#769f45";
+
+    // paper.style.backgroundColor = "#efefef";
+    // scissors.style.backgroundColor = "#efefef";
+});
+
+paper.addEventListener('click', () => {
+    let result = handleChoice("paper");
+    getComputerChoice();
+
+    console.log(result);
+    announcer.textContent = result;
+    player.textContent = humanScore;
+    computer.textContent = computerScore;
+
+    // rock.style.backgroundColor = "#efefef";
+    // paper.style.backgroundColor = "#ffcd66";
+    // scissors.style.backgroundColor = "#efefef";
+});
+
+scissors.addEventListener('click', () => {
+    let result = handleChoice("scissors");
+    getComputerChoice();
+
+    console.log(result);
+    announcer.textContent = result;
+    player.textContent = humanScore;
+    computer.textContent = computerScore;
+
+    // rock.style.backgroundColor = "#efefef";
+    // paper.style.backgroundColor = "#efefef";
+    // scissors.style.backgroundColor = "#ffcd66";
+});
+
+
+let buttons = document.querySelector(".buttons");
+let restart = document.querySelector(".restart");
+
+buttons.addEventListener('click', () => {
+    let currentHumanScore = Number(player.textContent);
+    let currentComputerScore = Number(computer.textContent);
+
+
+
+    if (currentComputerScore >= 5 || currentHumanScore >= 5) {
+        console.log("e");
+
+        if (currentComputerScore >= 5) {
+            announcer.textContent = "You lost the game.";
+        }
+        if (currentHumanScore >= 5) {
+            announcer.textContent = "You won the game!";
+        }
+
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+
+        rock.style.pointerEvents = "none";
+        paper.style.pointerEvents = "none";
+        scissors.style.pointerEvents = "none";
+
+        restart.style.display = "block";
+    }
+});
+
+restart.addEventListener('click', () => {
+    player.textContent = "0";
+    computer.textContent = "0";
+    humanScore = 0;
+    computerScore = 0;
+
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+
+    rock.style.pointerEvents = "auto";
+    paper.style.pointerEvents = "auto";
+    scissors.style.pointerEvents = "auto";
+
+    restart.style.display = "none";
+
+    announcer.textContent = "First to score 5 points wins!";
+});
 
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     let result;
 
-    if (randomNumber < 35) {
-        result = "rock";
-    }
-    else if (randomNumber > 68) {
-        result = "paper";
-    }
-    else {
-        result = "scissors";
-    }
+    if (randomNumber < 35)      result = "rock";
+    else if (randomNumber > 68) result = "paper";
+    else                        result = "scissors";
 
-    return result;
+    computerSelection = result;
+    return computerSelection;
 }
 
-function getUserChoice() {
-    let message = prompt(`Type "rock", "paper", or "scissors"`);
-    message = message.toLowerCase();
-    return message;
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function handleChoice(userChoice) {
+    let result = "";
 
     function playRound(humanChoice, computerChoice) {
-        humanChoice = humanSelection;
-        // console.log(humanChoice);
-        computerChoice = computerSelection;
-        // console.log(computerChoice);
-        let result;
+        humanChoice = userChoice;
 
         if (humanChoice === computerChoice) {
-            result = `Tie! You both made the same choice.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `Tie! You both made the same move.`;
         }
         if (humanChoice === "rock" && computerChoice === "paper") {
             computerScore += 1;
-            result = `You lose! Paper beats rock.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You lost! Computer chose paper.`;
         }
         if (humanChoice === "paper" && computerChoice === "scissors") {
             computerScore += 1;
-            result = `You lose! Scissors beat paper.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You lost! Computer chose scissors.`;
         }
         if (humanChoice === "scissors" && computerChoice === "rock") {
             computerScore += 1;
-            result = `You lose! Rock beats scissors.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You lost! Computer chose rock.`;
         }
 
         if (humanChoice === "rock" && computerChoice === "scissors") {
             humanScore += 1;
-            result = `You win! Rock beats scissors.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You won! Computer chose scissors.`;
         }
         if (humanChoice === "paper" && computerChoice === "rock") {
             humanScore += 1;
-            result = `You win! Paper beats rock.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You won! Computer chose rock.`;
         }
         if (humanChoice === "scissors" && computerChoice === "paper") {
             humanScore += 1;
-            result = `You win! Scissors beat paper.\nYour Score: ${humanScore}\nComputer's Score: ${computerScore}\n`;
+            result = `You won! Computer chose paper.`;
         }
 
-        humanSelection = getUserChoice();
-        computerSelection = getComputerChoice();
         return result;
     }
-    
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        console.log(playRound(humanSelection, computerSelection));
-    }
-
+    playRound(userChoice, getComputerChoice());
+    return result;
 }
-    
-playGame();
